76ab8abd711c93188d7d90539768a39ab115875f
dmartinez <damian.martinez@live.com.ar>
2019-02-28 14:38:54 -0300

    arreglos

---
+++ doc/precache-manifest.5d6145a000ec8c9719e4d795757d6c65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9b3a2aa1a9673fffe61a",
+    "url": "/static/js/main.9b3a2aa1.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "9b3a2aa1a9673fffe61a",
+    "url": "/static/css/main.04c7959d.chunk.css"
+  },
+  {
+    "revision": "d73321cc363ec665508d1e2430eb0336",
+    "url": "/index.html"
+  }
+];
